7773e8f640d63c6ba33196195e6ee39e98ae1bd2
Eleni Taki <elenitaki@col.vueling.com>
2020-08-16 17:00:59 +0200

    Updates

---
+++ precache-manifest.860ae133904aa180e607bf3b73187478.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "274e8291310bd7095161fc845b8e6e8d",
+    "url": "/react-tetris/index.html"
+  },
+  {
+    "revision": "c409cdf8538291a63f05",
+    "url": "/react-tetris/static/css/main.8eddb6b2.chunk.css"
+  },
+  {
+    "revision": "9c5d233e2529d24b4b3d",
+    "url": "/react-tetris/static/js/2.912f8b89.chunk.js"
+  },
+  {
+    "revision": "c409cdf8538291a63f05",
+    "url": "/react-tetris/static/js/main.6063d9ea.chunk.js"
+  },
+  {
+    "revision": "d038308b9c29dee2c93e",
+    "url": "/react-tetris/static/js/runtime~main.b208c46f.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/react-tetris/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/react-tetris/static/media/bg.2810fe6b.png"
+  }
+]);
